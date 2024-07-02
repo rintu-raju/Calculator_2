20c01481fa89f33741bfdbc331e3702a7d280dcb
rintu-raju <rintu110064@gmail.com>
2024-07-02 21:37:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+// Calculator program
+
+const display = document.getElementById("display");
+
+function appendToDisplay(input) {
+    display.value += input;
+}
+
+function clearDisplay() {
+    display.value = "";
+}
+
+function calculate() {
+    try {
+        const result = new Function('return ' + display.value)();
+        display.value = result;
+    } catch (error) {
+        display.value = "Error";
+    }
+}
